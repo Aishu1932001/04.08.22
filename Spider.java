adaca5940bd59db7f42184f9d004d1ec927e83cf
Aishwarya D <110168081+Aishu1932001@users.noreply.github.com>
2022-08-05 11:56:11 +0530

    Add files via upload

---
+++ Spider.java
@@ -0,0 +1,15 @@
+package com.abstract.Spider.demo;
+
+public class Spider extend Animal {
+    
+  public Spider() 
+    {
+       
+    super(8);
+    }
+   public void  eat()
+
+    {
+        
+        system.out.println( "The spider eat insects"");
+    }
