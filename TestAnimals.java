6410bdc9d8252672f124e41d64bc4f9a95eaa633
Aishwarya D <110168081+Aishu1932001@users.noreply.github.com>
2022-08-05 11:58:01 +0530

    Add files via upload

---
+++ TestAnimals.java
@@ -0,0 +1,40 @@
+package com.abstract.TestAnimals.demo;
+
+public class TestAnimals {
+    
+    public static void main(String[] args) {
+       
+  	      Fish d = new Fish();	
+        
+        Cat c = new Cat("Fluffy");		
+        
+        Animal a = new Fish();		
+      
+        Animal e = new Spider();	 	
+        
+        Pet p = new Cat();			
+        
+        d.play();		
+        
+        d.walk();		
+        
+        d.eat();				
+      
+        c.play();		
+        
+        c.getName();						
+       
+        a.eat();		
+        
+        e.eat();				
+        
+        a.walk();		
+      
+        e.walk();				
+    
+        p.play();				
+    }	
+
+}
+
+
