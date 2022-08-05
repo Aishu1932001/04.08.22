fe8842f6e9543c92563d200a2bd3b4b09bb4b8c6
Aishwarya D <110168081+Aishu1932001@users.noreply.github.com>
2022-08-05 11:53:41 +0530

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,32 @@
+package com.abstract.Cat.demo;
+
+public class Cat {
+    
+    public Cat extends Animal implement Pet(){
+        public string Petname;
+    }
+   public Cat()
+    {
+        this(" ");
+    }
+   public Cat ( string name);
+    {
+        super(4);
+        Catname= name;
+        }
+ //   @override//
+    public void eat(){
+        system.out.println( "Cat eat fishes");
+        }
+    public string getName(){
+        
+    return catName;
+        }
+    public void setName(string name);
+    {
+        CatName=name;
+        }
+    public void play(){
+        
+        throw newUnexpectedOperationException("Not supported yet");
+        }
