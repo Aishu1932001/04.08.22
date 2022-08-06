34e5b57c0a6497118a9637ed9e06bbaf6df2a06e
Aishwarya D <110168081+Aishu1932001@users.noreply.github.com>
2022-08-06 09:21:15 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,17 @@
+package com.abstract.Animal.demo;
+
+public class Animal {
+   
+    protected int legs;
+    protected Animal ( int legs );
+    {
+        this.legs=legs;
+    }
+    public  void walk()
+    {
+        
+        system.out.println( this animal walks by legs " + legs +");
+       
+    }
+    public  abstract void eat();
+}
